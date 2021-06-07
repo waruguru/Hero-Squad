3c6aef84457d7373e10c357a94db6005a6f68b5c
Joyce Muriithi <muriithi.joyce@eclectics.io>
2021-06-07 10:29:06 +0300

    Add variables in SquadClass

--- src/main/java/SquadClass.java
+++ src/main/java/SquadClass.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SquadClass {
+
+    private  String mName;
+    private Integer mMaxSize;
+    private static  ArrayList<SquadClass>instances =new ArrayList<>();
+    private int mID;
+    private List<HeroClass> mHeros;
+
+
 }
